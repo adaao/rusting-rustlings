545caa86a66a8948a9f5d0c0390f2b290baf4cd3
Adaão Ribeiro Mascarelli <armascarelli@gmail.com>
2023-05-11 09:49:55 +0000

    Variables exercises

---
+++ src/variables/variables5.rs
@@ -0,0 +1,13 @@
+// variables5.rs
+// Execute `rustlings hint variables5` or use the `hint` watch subcommand for a hint.
+
+// I AM DONE
+
+fn main() {
+    let number = "T-H-R-E-E"; // don't change this line
+    println!("Spell a Number : {}", number);
+    let mut number = "3"; // don't rename this variable
+    println!("Number plus two is : {}", number.to_owned() + "2");
+}
+
+//I'm not sure what this code should do
